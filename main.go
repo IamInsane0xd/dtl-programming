65e82ea2633ec2de99f13073c86ec7176c34d985
Biczo Ferenc <biczo.ferenc001@gmail.com>
2025-05-20 14:08:07 +0200

    refactor main.go to remove dotenv loading and implement a basic API endpoint

--- main.go
+++ main.go
@@ -1,33 +1,18 @@
 package DTLProgramming
 
 import (
-	"fmt"
-	"github.com/joho/godotenv"
-	"log"
-	"os"
-
-	"DTLProgramming/dbquery"
+	"github.com/gin-gonic/gin"
 )
 
+//goland:noinspection GoUnusedFunction
 func main() {
-	if err := godotenv.Load(); err != nil {
-		fmt.Println("Unable to find .env file in go project root, using system env values.")
-	}
+	router := gin.Default()
 
-	dbUser := os.Getenv("DB_USER")
-	dbPass := os.Getenv("DB_PASS")
-	dbAddr := os.Getenv("DB_ADDR")
-	dbName := os.Getenv("DB_NAME")
+	api := router.Group("/api")
 
-	err := dbquery.ConnectToDb(dbUser, dbPass, dbAddr, dbName)
-
-	if err != nil {
-		log.Fatalf("error: %s\n", err.Error())
-	}
-
-	// router := gin.Default()
-	//
-	// api := router.Group("/api")
-	//
-	// apiAuth := api.Group("/auth")
+	api.GET("/hello", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World",
+		})
+	})
 }
